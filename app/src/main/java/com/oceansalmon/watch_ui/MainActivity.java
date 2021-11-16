4efb94a85221fc78bc5f96ea228b6f1df72203b9
Ohgyuchan <gyu3637@gmail.com>
2021-11-16 15:57:08 +0900

    Create watch_ui

---
+++ app/src/main/java/com/oceansalmon/watch_ui/MainActivity.java
@@ -0,0 +1,23 @@
+package com.oceansalmon.watch_ui;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+import com.oceansalmon.watch_ui.databinding.ActivityMainBinding;
+
+public class MainActivity extends Activity {
+
+    private TextView mTextView;
+    private ActivityMainBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        mTextView = binding.text;
+    }
+}
