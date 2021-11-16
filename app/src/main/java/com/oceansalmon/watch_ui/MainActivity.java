b84ea1762d28daf2aab29ba7a1049848363c3165
Ohgyuchan <gyu3637@gmail.com>
2021-11-16 17:08:23 +0900

    Update certificate_screen

--- app/src/main/java/com/oceansalmon/watch_ui/MainActivity.java
+++ app/src/main/java/com/oceansalmon/watch_ui/MainActivity.java
@@ -1,7 +1,10 @@
 package com.oceansalmon.watch_ui;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 import com.oceansalmon.watch_ui.databinding.ActivityMainBinding;
@@ -18,6 +21,15 @@ public class MainActivity extends Activity {
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
-        mTextView = binding.text;
+        mTextView = binding.playRoutine;
+        Button imageButton = (Button) findViewById(R.id.btn_next);
+        imageButton.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), Certificate.class);
+                startActivity(intent);
+            }
+        });
     }
 }
